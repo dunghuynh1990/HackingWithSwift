d87c10ecaec450ebdda4277a4fcdb063320ae2d6
Huỳnh Trí Dũng <dunghuynh1990@gmail.com>
2017-01-11 21:09:58 +0700

    Add project 1, 2

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Huynh Tri Dung on 1/11/17.
+//  Copyright © 2017 Huynh Tri Dung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
